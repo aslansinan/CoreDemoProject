52ae9e503731a87f6661746dfcd061907f0a17a5
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-04 16:22:54 +0300

    login base sayfa

---
+++ CoreDemo/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult index()
+    {
+        return View();
+    }
+}
