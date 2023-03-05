2232f28cbe1b91cad52fb9081a3246845c028198
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-05 15:11:29 +0300

    adding about page

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete;
+
+public class AboutManager : IAboutService
+{
+    IAboutDal _aboutDal;
+
+    public AboutManager(IAboutDal aboutDal)
+    {   
+        _aboutDal = aboutDal;
+    }
+
+
+    public List<About> GetList()
+    {
+        return _aboutDal.GetListAll();
+    }
+}
---
+++ CoreDemo/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers;
+
+public class AboutController : Controller
+{
+    AboutManager am = new AboutManager(new EfAboutRepository());
+
+    public IActionResult index()
+    {
+        return View();
+    }
+
+    public PartialViewResult SocialMediaAbout()
+    {
+        var values = am.GetList();
+        return PartialView(values);
+    }
+}
