57ee561acb1736aa70d73f6702a7678b58ca8342
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-06 09:18:13 +0300

    Authorize first step

---
+++ CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers;
+[Authorize]
+public class WriterController : Controller
+{
+    public IActionResult index()
+    {
+        return View();
+    }
+    public IActionResult WriterProfile()
+    {
+        return View();
+    }
+    public IActionResult WriterMail()
+    {
+        return View();
+    }
+}
