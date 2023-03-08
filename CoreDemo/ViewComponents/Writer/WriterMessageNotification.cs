42fd541cab57bb90c915bb65d95da0f220c6c345
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-08 17:43:21 +0300

    notifications with viewcomponent

---
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Writer;
+
+public class WriterMessageNotification : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
