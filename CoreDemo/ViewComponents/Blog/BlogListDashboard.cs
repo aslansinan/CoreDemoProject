e987a02b44b1979e39fbf23f6a5735e3e93f1082
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-09 15:16:14 +0300

    WriterDashboard added

---
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Category;
+
+public class BlogListDashboard : ViewComponent
+{
+    private BlogManager bm = new BlogManager(new EfBlogRepository());
+
+    public IViewComponentResult Invoke()
+    {
+        var values = bm.GetBlogListWithCategory();
+        return View(values);
+    }
+}
