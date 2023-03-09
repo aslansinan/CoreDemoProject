ee2cfa78c35042e9cb3bd58b3381095f47f6e15c
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-09 21:34:29 +0300

    istatistikler Dashboarda eklendi

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers;
@@ -7,6 +8,10 @@ public class DashboardController : Controller
 {
     public IActionResult index()
     {
+        Context c = new Context();
+        ViewBag.v1 = c.Blogs.Count().ToString();
+        ViewBag.v2 = c.Blogs.Where(x=>x.WriterID==1).Count().ToString();
+        ViewBag.v3 = c.Categories.Count().ToString();
         return View();
     }
 }
