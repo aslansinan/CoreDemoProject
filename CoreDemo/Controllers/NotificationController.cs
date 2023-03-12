58e1bec3cf0d5b304f1302a4da72a94d64e4ad8e
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-12 11:49:22 +0300

    Bildirimler listeleme sayfası eklendi

--- CoreDemo/Controllers/NotificationController.cs
+++ CoreDemo/Controllers/NotificationController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers;
 
+[AllowAnonymous]
 public class NotificationController : Controller
 {
+    private NotificationManager _notificationManager = new NotificationManager(new EfNotificationRepository());
     public IActionResult Index()
     {
         return View();
+    }
+
+    public IActionResult AllNotification()
+    {
+        var values = _notificationManager.GetList();
+        return View(values);
     }
 }
