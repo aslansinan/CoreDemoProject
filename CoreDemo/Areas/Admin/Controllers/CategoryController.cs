6b0c0de2712c912c1feaa3334a30ceb4bff4873c
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-14 16:30:02 +0300

    Get All Categories in admin area

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers;
 
 public class CategoryController : Controller
 {
+    private CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
     [Area("Admin")]
     public IActionResult Index()
     {
-        return View();
+        var values = _categoryManager.GetList();
+        return View(values);
     }
 }
