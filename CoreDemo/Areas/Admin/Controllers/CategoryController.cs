3d2ca4f610ec614f9fce244bbefb2c251dfcbfeb
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-14 16:49:03 +0300

    importing paginator in admin area

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace CoreDemo.Areas.Admin.Controllers;
 
@@ -8,9 +9,9 @@ public class CategoryController : Controller
 {
     private CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
     [Area("Admin")]
-    public IActionResult Index()
+    public IActionResult Index(int page=1)
     {
-        var values = _categoryManager.GetList();
+        var values = _categoryManager.GetList().ToPagedList(page,3);
         return View(values);
     }
 }
