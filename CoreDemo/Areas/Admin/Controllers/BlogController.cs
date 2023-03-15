0d8cb55894922812c0b69f930f74b7be50f88763
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-15 13:00:23 +0300

    Dynamic export işlemi db den veri alma

--- CoreDemo/Areas/Admin/Controllers/BlogController.cs
+++ CoreDemo/Areas/Admin/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using ClosedXML.Excel;
 using CoreDemo.Areas.Admin.Models;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers;
@@ -45,6 +46,53 @@ public class BlogController : Controller
     }
 
     public IActionResult BlogListExcel()
+    {
+        return View();
+    }
+
+    public IActionResult ExportDinamicExcelBlogList()
+    {
+        using (var workbook = new XLWorkbook())
+        {
+            var worksheet = workbook.Worksheets.Add("BlogListesi");
+            worksheet.Cell(1, 1).Value = "Blog Id";
+            worksheet.Cell(1, 2).Value = "Blog Adı";
+
+            int BlogRowCount = 2;
+            foreach (var item in BlogTitleList())
+            {
+                worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                BlogRowCount++;
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                workbook.SaveAs(stream);
+                var content = stream.ToArray();
+                return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    "Calisma2.xlsx");
+            }
+
+        }
+    }
+
+    public List<BlogModel2> BlogTitleList()
+    {
+        List<BlogModel2> _list = new List<BlogModel2>();
+        using (var c = new Context())
+        {
+            _list = c.Blogs.Select(x => new BlogModel2
+            {
+                ID = x.BlogID,
+                BlogName = x.BlogTitle
+            }).ToList();
+        }
+
+        return _list;
+    }
+
+    public IActionResult BlogTitleListExcel()
     {
         return View();
     }
---
+++ CoreDemo/Areas/Admin/Models/BlogModel2.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreDemo.Areas.Admin.Models;
+
+public class BlogModel2
+{
+    public int ID { get; set; }
+    public string BlogName { get; set; }
+}
