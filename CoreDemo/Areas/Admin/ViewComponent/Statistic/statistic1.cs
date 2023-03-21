225cca312ea1a12fce1357a164a59396847f3aa1
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-21 10:48:22 +0300

    implement wheather with xml

--- CoreDemo/Areas/Admin/ViewComponent/Statistic/statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponent/Statistic/statistic1.cs
@@ -1,4 +1,5 @@
-﻿using BusinessLayer.Concrete;
+﻿using System.Xml.Linq;
+using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,11 @@ public class statistic1 : Microsoft.AspNetCore.Mvc.ViewComponent
         ViewBag.v1 = _blogManager.GetList().Count;
         ViewBag.v2 = _context.Contacts.Count();
         ViewBag.v3 = _context.Comments.Count();
+        string api = "af954833f1a0cfe37c9cef6f40c5844e";
+        string connection =
+            "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&appid=" + api;
+        XDocument document = XDocument.Load(connection);
+        ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value")?.Value;
         return View();
     }
 }
