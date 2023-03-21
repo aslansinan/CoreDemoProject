dde7006fe4855570bddfbbea52dea2c4f7447ae7
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-21 11:38:28 +0300

    implemnt static chart

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,33 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers;
+[Area("Admin")]
+public class ChartController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult CategoryChart()
+    {
+        List<CategoryClass> list = new List<CategoryClass>();
+        list.Add(new CategoryClass
+        {
+            CategoryName = "Teknolji",
+            CategoryCount = 10
+        });
+        list.Add(new CategoryClass
+        {
+            CategoryName = "Spor",
+            CategoryCount = 44
+        });
+        list.Add(new CategoryClass
+        {
+            CategoryName = "Müzik",
+            CategoryCount = 13
+        });
+        return Json(new { jsonList = list });
+    }
+}
