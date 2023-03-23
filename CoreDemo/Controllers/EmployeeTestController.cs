53f62803cd33426a54265d81ebbcaa01bf32d896
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-23 12:57:08 +0300

    arayüzden apide veri silme ve güncelleme

--- CoreDemo/Controllers/EmployeeTestController.cs
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -36,6 +36,47 @@ public class EmployeeTestController : Controller
         return View(class1);
     }
 
+    [HttpGet]
+    public async Task<IActionResult> EditEmployee(int id)
+    {
+        var httpClient = new HttpClient();
+        var responseMessage = await httpClient.GetAsync($"https://localhost:7219/api/Default/" + id);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonEmployee = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<Class1>(jsonEmployee);
+            return View(values);
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EditEmployee(Class1 class1)
+    {
+        var httpClient = new HttpClient();
+        var jsonEmployee = JsonConvert.SerializeObject(class1);
+        var content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+        var responseMessage = await httpClient.PutAsync("https://localhost:7219/api/Default", content);
+        
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View(class1);
+        
+    }
+
+    public async Task<IActionResult> DeleteEmployee(int id)
+    {
+        var httpClient = new HttpClient();
+        var responseMessage = await httpClient.DeleteAsync($"https://localhost:7219/api/Default/" + id);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");  
+        }
+        return View();
+    }
     public class Class1
     {
         public int Id { get; set; }
