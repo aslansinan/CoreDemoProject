7d71a4fc63e8cc34f4fabf4f5b9199ce7be5b076
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-23 12:15:07 +0300

    Api ile dbler arası haberleşme ve veri getirme , kayıt ekleme

---
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CoreDemo.Controllers;
+
+public class EmployeeTestController : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        var httpClient = new HttpClient();
+        var responseMessage = await httpClient.GetAsync("https://localhost:7219/api/Default");
+        var jsonString = await responseMessage.Content.ReadAsStringAsync();
+        var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+
+        return View(values);
+    }
+
+    public IActionResult AddEmployee()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddEmployee(Class1 class1)
+    {
+        var httpClient = new HttpClient();
+        var jsonEmployee = JsonConvert.SerializeObject(class1);
+        StringContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+        var responseMessage = await httpClient.PostAsync("https://localhost:7219/api/Default", content);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View(class1);
+    }
+
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
