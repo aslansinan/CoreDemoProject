d4bfbc85b30ad21068b0cce94d000fd59936b37b
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-23 09:28:49 +0300

    api layer added

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer;
+
+public class Context : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("server=.;database=CoreBlogApiDb;integrated security = true; Trust Server Certificate=true");
+    }
+    
+    public DbSet<Employee> Employees { get; set; }
+} 
---
+++ BlogApiDemo/DataAccessLayer/Employee.cs
@@ -0,0 +1,7 @@
+﻿namespace BlogApiDemo.DataAccessLayer;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
