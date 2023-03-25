cbda751c8d742ed318bf876747e60971d6db39f0
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-26 00:33:59 +0300

    son düzeltmeler

--- CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,13 +7,15 @@ namespace CoreDemo.ViewComponents.Writer;
 
 public class WriterMessageNotification : ViewComponent
 {
+    Context context = new Context();
     Message2Manager _message2Manager = new Message2Manager(new EfMessage2Repository());
 
     public IViewComponentResult Invoke()
     {
-        int id = 3;
-        var values = _message2Manager.GetInboxListByWriter(id);
-        //var values = _message2Manager.GetList();
+        var username = User.Identity?.Name;
+        var userMail = context.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+        var writerId = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterId).FirstOrDefault();
+        var values = _message2Manager.GetInboxListByWriter(writerId);
         return View(values);
     }
 }
