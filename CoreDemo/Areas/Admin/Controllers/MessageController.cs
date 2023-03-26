4a48038998dd8215083ab46c34e1aa42cabb89e1
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-26 23:52:36 +0300

    Admin dashboard static değişiklikler ve mesaj sayfası statik eklenmesi

---
+++ CoreDemo/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,13 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class MessageController : Controller
+{
+    public IActionResult Inbox()
+    {
+        return View();
+    }
+}
